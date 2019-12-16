8c6aebfdc9ae73efc5d98facf6f83f93ea4e0842
jiwoong <jiwoong@cardoc.kr>
2019-12-16 17:58:05 +0900

    chore :  - Change minSdkVersion from 14 to 16  - Change gradle version from 3.2.1 to 3.5.3  - Change library version

--- library/src/main/java/com/antonyt/infiniteviewpager/InfiniteViewPager.java
+++ library/src/main/java/com/antonyt/infiniteviewpager/InfiniteViewPager.java
@@ -2,8 +2,8 @@ package com.antonyt.infiniteviewpager;
 
 
 import android.content.Context;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import android.util.AttributeSet;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Scroller;
